6cdddd94a542fb049b63def516d397f938bb0373
Priates of Zimsey <realcoder2023@gmail.com>
2025-09-16 19:04:09 -0400

    Add project files.

---
+++ Converter/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Converter
+{
+
+    class App
+    {
+
+        private static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+
+                Console.WriteLine("Enter Hex: ");
+                int colorR = Convert.ToInt16(Console.ReadLine());
+
+                string colorCode = createConvert(colorR);
+
+                Console.Write(colorCode);
+                Console.Write(colorCode);
+                Console.Write(colorCode);
+            }
+
+            Console.ReadKey();
+
+
+
+        }
+
+        private static string createConvert(int a)
+        {
+            return Convert.ToString(a, 16);
+        }
+
+        
+
+    }
+    
+}
