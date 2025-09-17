0d5bc8f17b45dea3e9a82af6c20f8a167d44476b
Priates of Zimsey <realcoder2023@gmail.com>
2025-09-16 21:40:48 -0400

    added mongodb client code

--- Converter/Program.cs
+++ Converter/Program.cs
@@ -1,32 +1,52 @@
-﻿namespace Converter
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Drawing;
+
+namespace Converter
 {
 
+    
     class App
     {
 
         private static void Main(string[] args)
         {
             if(args.Length == 0)
-            {
+            {                
 
+          
                 Console.WriteLine("Enter Hex: ");
                 int colorR = Convert.ToInt16(Console.ReadLine());
 
-                string colorCode = createConvert(colorR);
+                string colorCode = CreateConvert(colorR);
 
                 Console.Write(colorCode);
                 Console.Write(colorCode);
                 Console.Write(colorCode);
+
+                string ColorList = $"# {colorCode}, {colorCode}, {colorCode}";
+
+                Doc(ColorList);
+
             }
 
             Console.ReadKey();
         }
 
-        private static string createConvert(int a)
+        private static string CreateConvert(int a)
         {
             return Convert.ToString(a, 16);
         }
 
+
+        private async static void Doc(string doc)
+        {
+            const string URL = "";
+            var client = new MongoClient(URL);
+            var db = client.GetDatabase("Color");
+            var coll = db.GetCollection<BsonDocument>("color");
+            await coll.InsertOneAsync(new BsonDocument("color", doc));
+        }
     }
     
 }
